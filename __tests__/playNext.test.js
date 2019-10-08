80539b1892ae4c4f95c833e653e17ddfc8283b9b
spacekitcat <spacekitcat@spacekitcatsMBP.fritz.box>
2019-10-08 17:59:50 +0100

    chore(tests/playNext): Adding a test for two items .

--- __tests__/playNext.test.js
+++ __tests__/playNext.test.js
@@ -1,33 +1,33 @@
-import playNext from '../src/playNext';
-import play from 'play-on-apple-tv';
-import findPlayableFiles from '../src/findPlayableFiles';
-import randomWrapper from '../src/randomWrapper';
+import playNext from "../src/playNext";
+import play from "play-on-apple-tv";
+import findPlayableFiles from "../src/findPlayableFiles";
+import randomWrapper from "../src/randomWrapper";
 
-jest.mock('play-on-apple-tv');
-jest.mock('../src/findPlayableFiles');
-jest.mock('../src/randomWrapper');
+jest.mock("play-on-apple-tv");
+jest.mock("../src/findPlayableFiles");
+jest.mock("../src/randomWrapper");
 
-describe('The `playNext` module', () => {
-  describe('when the returned list has one item', () => {
-    const specifiedMediaFolder = '/my/films';
-    const specifiedAppleTvAddress = '192.168.4.7';
+describe("The `playNext` module", () => {
+  describe("when the returned list has two item and the random function returns 0", () => {
+    const specifiedMediaFolder = "/my/films";
+    const specifiedAppleTvAddress = "192.168.4.7";
 
     beforeAll(async () => {
-      findPlayableFiles.mockResolvedValueOnce(['/my/films/a-file.mp4']);
+      findPlayableFiles.mockResolvedValueOnce(["/my/films/a-file.mp4", "/my/films/b-file.mp4"]);
       randomWrapper.mockReturnValueOnce(0);
 
       await playNext(specifiedMediaFolder, specifiedAppleTvAddress);
     });
 
-    it('should call the `findPlayableFiles` method', async () => {
+    it("should call the `findPlayableFiles` method", async () => {
       expect(findPlayableFiles).toHaveBeenCalledWith(specifiedMediaFolder);
     });
 
-    it('should call the `randomWrapper` method', async () => {
-      expect(randomWrapper).toHaveBeenCalledWith(0, 0);
+    it("should call the `randomWrapper` method", async () => {
+      expect(randomWrapper).toHaveBeenCalledWith(0, 1);
     });
 
-    it('should call the `findPlayableFiles` method', async () => {
+    it("should call the `findPlayableFiles` method", async () => {
       expect(play).toHaveBeenCalledWith(
         `/my/films/a-file.mp4`,
         specifiedAppleTvAddress,
@@ -36,9 +36,65 @@ describe('The `playNext` module', () => {
     });
   });
 
-  describe('when the returned list has zero item', () => {
-    const specifiedMediaFolder = '/my/films';
-    const specifiedAppleTvAddress = '192.168.4.7';
+  describe("when the returned list has two item and the random function returns 1", () => {
+    const specifiedMediaFolder = "/my/films";
+    const specifiedAppleTvAddress = "192.168.4.7";
+
+    beforeAll(async () => {
+      findPlayableFiles.mockResolvedValueOnce(["/my/films/a-file.mp4", "/my/films/b-file.mp4"]);
+      randomWrapper.mockReturnValueOnce(1);
+
+      await playNext(specifiedMediaFolder, specifiedAppleTvAddress);
+    });
+
+    it("should call the `findPlayableFiles` method", async () => {
+      expect(findPlayableFiles).toHaveBeenCalledWith(specifiedMediaFolder);
+    });
+
+    it("should call the `randomWrapper` method", async () => {
+      expect(randomWrapper).toHaveBeenCalledWith(0, 1);
+    });
+
+    it("should call the `findPlayableFiles` method", async () => {
+      expect(play).toHaveBeenCalledWith(
+        `/my/films/b-file.mp4`,
+        specifiedAppleTvAddress,
+        null
+      );
+    });
+  });
+
+  describe("when the returned list has one item", () => {
+    const specifiedMediaFolder = "/my/films";
+    const specifiedAppleTvAddress = "192.168.4.7";
+
+    beforeAll(async () => {
+      findPlayableFiles.mockResolvedValueOnce(["/my/films/a-file.mp4"]);
+      randomWrapper.mockReturnValueOnce(0);
+
+      await playNext(specifiedMediaFolder, specifiedAppleTvAddress);
+    });
+
+    it("should call the `findPlayableFiles` method", async () => {
+      expect(findPlayableFiles).toHaveBeenCalledWith(specifiedMediaFolder);
+    });
+
+    it("should call the `randomWrapper` method", async () => {
+      expect(randomWrapper).toHaveBeenCalledWith(0, 0);
+    });
+
+    it("should call the `findPlayableFiles` method", async () => {
+      expect(play).toHaveBeenCalledWith(
+        `/my/films/a-file.mp4`,
+        specifiedAppleTvAddress,
+        null
+      );
+    });
+  });
+
+  describe("when the returned list has zero item", () => {
+    const specifiedMediaFolder = "/my/films";
+    const specifiedAppleTvAddress = "192.168.4.7";
 
     beforeEach(async () => {
       findPlayableFiles.mockResolvedValueOnce([]);
@@ -48,15 +104,15 @@ describe('The `playNext` module', () => {
       await playNext(specifiedMediaFolder, specifiedAppleTvAddress);
     });
 
-    it('should call the `findPlayableFiles` method', async () => {
+    it("should call the `findPlayableFiles` method", async () => {
       expect(findPlayableFiles).toHaveBeenCalledWith(specifiedMediaFolder);
     });
 
-    it('should NOT call the `randomWrapper` method', async () => {
+    it("should NOT call the `randomWrapper` method", async () => {
       expect(randomWrapper).not.toHaveBeenCalled();
     });
 
-    it('should NOT call the `findPlayableFiles` method', async () => {
+    it("should NOT call the `findPlayableFiles` method", async () => {
       expect(play).not.toHaveBeenCalled();
     });
   });
